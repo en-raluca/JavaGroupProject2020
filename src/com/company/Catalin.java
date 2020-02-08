90cbe6db13a7f030988be936d4a03bfb89b5296a
Cata-PPA <popa.catalin.ioan@gmail.com>
2020-02-08 14:37:07 +0200

    iar am facut ceva

--- src/com/company/Catalin.java
+++ src/com/company/Catalin.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Catalin {
     public static void main(String[] args){
-        System.out.println("Catalin din nou");
+        System.out.println("Cataliiiin din nou");
     }
 }
