fc8c68b713caa7a6b034e5bca771919ac17ab776
Sirbu Dan Alexandru <alex@ALexs-MacBook-Pro.local>
2020-02-08 14:48:06 +0200

    mesaj

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Raluca CHANGE FROM ui");
-
         System.out.println("Andrei");
         System.out.println("Bogdan");
         System.out.println("Alex");
+        System.out.println("Salut lume");
 	// write your code here
     }
 }
