251c64bc3d2ba0b09b3a9c37e93e81f71a5bb1a3
Porumb <ionut.porumb@siemens.com>
2020-02-08 13:12:14 +0200

    Commit from UI branch.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Raluca");
+        System.out.println("Raluca CHANGE FROM ui");
 	// write your code here
     }
 }
