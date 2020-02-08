45f8c3c7d032c493b74d4d9825aa1a00b7658e99
Andrei <rus_andrei2016@yahoo.com>
2020-02-08 13:52:43 +0200

    Am adaugat un nou println

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Raluca CHANGE FROM ui");
+        System.out.println("Andrei");
 	// write your code here
     }
 }
