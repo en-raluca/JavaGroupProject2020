4e052d6079d060a92ffc9a902c561294efd62e4e
IonutPorumb <ionut_porumb@yahoo.com>
2020-02-08 13:57:00 +0200

    Am modificat Main cu SOUT(Ionut Porumb)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Raluca");
+        System.out.println("Ionut Porumb");
 	// write your code here
     }
 }
