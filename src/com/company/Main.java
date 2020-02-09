db551926604cca59fe6d9e1779f5d815543d3f9b
IonutPorumb <ionut_porumb@yahoo.com>
2020-02-09 23:59:23 +0200

    Am mai adaugat un System.out.println(Ionut Porumb2)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Raluca");
         System.out.println("Ionut Porumb");
+        System.out.println("Ionut Porumb 2");
 	// write your code here
     }
 }
