3ca017dc6b279555af96f42fba2bb929320f6cbc
IonutPorumb <ionut_porumb@yahoo.com>
2020-02-14 21:51:31 +0200

    I added in class Ionut the method printFirst12FibonacciNo

--- src/com/company/Ionut.java
+++ src/com/company/Ionut.java
@@ -8,4 +8,15 @@ public class Ionut {
         System.out.println("Rezultatul sumei este: "+s);
     }
 
+    public static void printFirst12FibonacciNo(){
+        int [] arrayFibonacci=new int [12];
+        arrayFibonacci[0]=1;
+        arrayFibonacci[1]=1;
+        for(int i=0;i<=11;i++) {
+            if (i >= 2) {
+                arrayFibonacci[i] = arrayFibonacci[i - 1] + arrayFibonacci[i - 2];
+            }
+            System.out.println("The first 12 Fibonnacci elements are arrayFibonacci["+i+"] : " + arrayFibonacci[i]);
+        }
+    }
 }
