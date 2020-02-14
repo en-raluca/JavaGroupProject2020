f4f37a4a1228f633e230b9ce23c82d7b64cd5feb
AdrianaSlmn <adriana.solomon96@yahoo.com>
2020-02-14 22:19:22 +0200

    commit 2

--- src/com/company/Adriana.java
+++ src/com/company/Adriana.java
@@ -3,5 +3,30 @@ package com.company;
 public class Adriana {
     public static void main(String[] args) {
         System.out.println("Adriana");
+        int result= justForTest(18);
     }
-}
+
+
+        public static int justForTest(int param){
+            int t, a=20, b=10, c=b*2, d=a+2*b-c;
+            if(d>param){
+                t=11;
+            }
+            else{
+                t=22;
+            }
+            boolean boo=false;
+            while(t<13){
+                System.out.println(t);
+            }
+            t++;
+            int[] sir=new int[3];
+            sir[0]=0;
+            sir[1]=1;
+            sir[2]=2;
+            boo=sir.length==3;
+            return t*2;
+
+        }
+    }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Raluca");
         System.out.println("Ionut Porumb");
+        System.out.println("Adriana ");
 	// write your code here
     }
 }
